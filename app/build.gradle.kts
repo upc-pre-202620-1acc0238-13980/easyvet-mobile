4ec543217f749c7387e6669d6014968c1af5151e
jmayta1984 <jmayta@pucp.edu.pe>
2026-09-04 08:42:01 -0500

    feat: add Coil library dependencies for image loading and network handling

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,11 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    // Coil
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
+
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
