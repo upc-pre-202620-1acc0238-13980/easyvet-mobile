0257f47e18a5c9dc7a9113e08100cea4a11047f5
jmayta1984 <jmayta@pucp.edu.pe>
2026-09-11 08:04:51 -0500

    refactor(catalog): remove unused import for InMemoryRepository in GetProductsUseCase

--- app/src/main/java/pe/edu/upc/easyvet/features/catalog/application/GetProductsUseCase.kt
+++ app/src/main/java/pe/edu/upc/easyvet/features/catalog/application/GetProductsUseCase.kt
@@ -1,7 +1,6 @@
 package pe.edu.upc.easyvet.features.catalog.application
 
 import pe.edu.upc.easyvet.features.catalog.domain.ProductRepository
-import pe.edu.upc.easyvet.features.catalog.infrastructure.repositories.InMemoryRepository
 import pe.edu.upc.easyvet.features.catalog.infrastructure.repositories.ProductRepositoryImpl
 
 class GetProductsUseCase(private val repository: ProductRepository = ProductRepositoryImpl()) {
