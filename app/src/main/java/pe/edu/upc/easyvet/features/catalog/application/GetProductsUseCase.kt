428509f0770ed15c1d019e5e8b1b68f1e89a47f0
jmayta1984 <jmayta@pucp.edu.pe>
2026-09-11 08:03:12 -0500

    feat(catalog): implement ProductRepositoryImpl for remote product fetching and update GetProductsUseCase

--- app/src/main/java/pe/edu/upc/easyvet/features/catalog/application/GetProductsUseCase.kt
+++ app/src/main/java/pe/edu/upc/easyvet/features/catalog/application/GetProductsUseCase.kt
@@ -2,7 +2,8 @@ package pe.edu.upc.easyvet.features.catalog.application
 
 import pe.edu.upc.easyvet.features.catalog.domain.ProductRepository
 import pe.edu.upc.easyvet.features.catalog.infrastructure.repositories.InMemoryRepository
+import pe.edu.upc.easyvet.features.catalog.infrastructure.repositories.ProductRepositoryImpl
 
-class GetProductsUseCase(private val repository: ProductRepository = InMemoryRepository()) {
+class GetProductsUseCase(private val repository: ProductRepository = ProductRepositoryImpl()) {
     suspend operator fun invoke() = repository.getProducts()
 }
--- app/src/main/java/pe/edu/upc/easyvet/features/catalog/infrastructure/remote/ProductsResponseDto.kt
+++ app/src/main/java/pe/edu/upc/easyvet/features/catalog/infrastructure/remote/ProductsResponseDto.kt
@@ -1,6 +1,10 @@
 package pe.edu.upc.easyvet.features.catalog.infrastructure.remote
 
+import com.google.gson.annotations.SerializedName
+
 data class ProductsResponseDto(
+    @SerializedName("count")
     val count: Int,
+    @SerializedName("results")
     val products: List<ProductDto>
 )
