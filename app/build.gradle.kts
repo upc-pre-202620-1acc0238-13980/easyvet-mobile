d5ec1989762d13059f31a1587a236189500bad8c
jmayta1984 <jmayta@pucp.edu.pe>
2026-09-16 21:06:52 -0500

    chore: add Hilt navigation compose dependency in build.gradle.kts and update libs.versions.toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     // Hilt
     implementation(libs.hilt.android)
     ksp(libs.hilt.android.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
 
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
