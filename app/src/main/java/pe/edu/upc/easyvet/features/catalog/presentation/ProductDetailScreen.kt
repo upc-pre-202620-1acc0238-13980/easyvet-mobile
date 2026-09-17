e5c8c6df468c7e263908722423bb98f0fbcd2a2a
jmayta1984 <jmayta@pucp.edu.pe>
2026-09-16 21:06:58 -0500

    feat(catalog): add ProductDetailScreen and ViewModel for product detail retrieval

---
+++ app/src/main/java/pe/edu/upc/easyvet/features/catalog/presentation/ProductDetailScreen.kt
@@ -0,0 +1,59 @@
+package pe.edu.upc.easyvet.features.catalog.presentation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil3.compose.AsyncImage
+
+@Composable
+fun ProductDetailScreen(
+    id: Int,
+    modifier: Modifier,
+    viewModel: ProductDetailViewModel = viewModel(),
+    onBack: () -> Unit
+) {
+    val uiState = viewModel.uiState.collectAsStateWithLifecycle().value
+
+    LaunchedEffect(key1 = id) {
+        viewModel.loadProductById(id)
+    }
+
+    Scaffold(modifier = modifier) { paddingValues ->
+        Column(
+            modifier = Modifier.padding(paddingValues),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            when (uiState) {
+
+                is ProductDetailUiState.Loading -> {
+                    CircularProgressIndicator()
+                }
+
+                is ProductDetailUiState.Success -> {
+                    AsyncImage(
+                        model = uiState.product.imageUrl,
+                        contentDescription = uiState.product.name
+                    )
+
+                    Text(text = uiState.product.description)
+                }
+
+                is ProductDetailUiState.Error -> {
+                    Text(text = uiState.message)
+
+                }
+            }
+        }
+
+    }
+}
