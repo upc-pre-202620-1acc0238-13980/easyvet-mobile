def1b148a751ffcd0b417d9899cc0ade21a07a4c
jmayta1984 <jmayta@pucp.edu.pe>
2026-09-18 07:20:29 -0500

    refactor: rename AppNavHost package for improved clarity

--- app/src/main/java/pe/edu/upc/easyvet/navigation/AppNavHost.kt
+++ app/src/main/java/pe/edu/upc/easyvet/navigation/AppNavHost.kt
@@ -1,10 +1,10 @@
-package pe.edu.upc.easyvet.core
+package pe.edu.upc.easyvet.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import pe.edu.upc.easyvet.features.catalog.presentation.HomeRoute
-import pe.edu.upc.easyvet.features.catalog.presentation.catalogNavGraph
+import pe.edu.upc.easyvet.features.catalog.presentation.navigation.HomeRoute
+import pe.edu.upc.easyvet.features.catalog.presentation.navigation.catalogNavGraph
 
 @Composable
 fun AppNavHost(navController: NavHostController) {
