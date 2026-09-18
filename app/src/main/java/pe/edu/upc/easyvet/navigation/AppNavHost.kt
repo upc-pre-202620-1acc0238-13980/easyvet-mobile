c5d52ade51816f0a2303152fb7b02563062851c8
jmayta1984 <jmayta@pucp.edu.pe>
2026-09-18 07:29:51 -0500

    feat: integrate Scaffold and NavigationBar into AppNavHost for enhanced layout

--- app/src/main/java/pe/edu/upc/easyvet/navigation/AppNavHost.kt
+++ app/src/main/java/pe/edu/upc/easyvet/navigation/AppNavHost.kt
@@ -1,15 +1,30 @@
 package pe.edu.upc.easyvet.navigation
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import pe.edu.upc.easyvet.features.catalog.presentation.navigation.HomeRoute
 import pe.edu.upc.easyvet.features.catalog.presentation.navigation.catalogNavGraph
 
 @Composable
-fun AppNavHost(navController: NavHostController) {
+fun AppNavHost(navController: NavHostController, modifier: Modifier = Modifier) {
 
-    NavHost(navController = navController, startDestination = HomeRoute) {
-        catalogNavGraph(navController)
+    Scaffold(
+        bottomBar = {
+            NavigationBar { }
+        }
+    ) { paddingValues ->
+        NavHost(
+            navController = navController,
+            startDestination = HomeRoute,
+            modifier = modifier.padding(paddingValues)
+        ) {
+            catalogNavGraph(navController)
+        }
     }
+
 }
